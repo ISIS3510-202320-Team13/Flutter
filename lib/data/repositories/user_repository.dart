738dc44e19fe2308b0825d333673d4447cedf9e3
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2023-10-12 00:58:27 -0500

    Add Bloc implementation of Authentication
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

---
+++ lib/data/repositories/user_repository.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:parkez/data/models/user.dart';
+
+class UserRepository {
+  final firebase_auth.FirebaseAuth _firebaseAuth;
+
+  UserRepository({firebase_auth.FirebaseAuth? firebaseAuth})
+      : _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
+
+  Future<User?> getUser() async {
+    return _firebaseAuth.currentUser?.toUser;
+  }
+}
+
+extension on firebase_auth.User {
+  User get toUser {
+    return User(id: uid, email: email, name: displayName, photo: photoURL);
+  }
+}
