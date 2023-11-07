b1706aedc9869ca3e928b9438c3a7ab6292a0c3f
Juan Camilo Reyes <j,reyesc@uniandes.edu.co>
2023-11-06 22:06:02 -0500

    added new api calls

---
+++ lib/logic/calls/apiCall.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiCall {
+  final String baseUrl = 'http://api.parkez.xyz:8082/';
+
+  Future<List<dynamic>> fetch(String endpoint) async {
+    final response = await http.get(Uri.parse('$baseUrl$endpoint'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data;
+    } else {
+      throw Exception('Failed to load entity');
+    }
+  }
+
+  Future<String> create(String endpoint, Map<String,String> data) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl$endpoint'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(data)
+    );
+
+    if (response.statusCode == 201) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to create entity');
+    }
+  }
+
+  Future<String> update(String endpoint, String uid, Map<String,String> data) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl$endpoint/$uid'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(data),
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to update entity');
+    }
+  }
+
+
+}
+
